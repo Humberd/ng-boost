2cc48989cec2547bb9b5215b99e7c8352ee24e3e
Humberd <humberd2@gmail.com>
2019-06-15 22:57:22 +0200

    Added second mode tests

--- projects/ng-boost/src/lib/refresher/autorefresh.spec.ts
+++ projects/ng-boost/src/lib/refresher/autorefresh.spec.ts
@@ -1,67 +1,231 @@
 import { of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { delay, tap } from 'rxjs/operators';
 import { autorefresh, AutorefreshMode } from './autorefresh';
 import { fakeAsync, tick } from '@angular/core/testing';
 
 describe('AutorefreshConsumerImpl', () => {
-  it('should autorefresh every 1 second', fakeAsync(() => {
-    let received = null;
-    const obs = of('foo')
-      .pipe(
-        tap(it => received = it)
-      );
-    const consumer = autorefresh({
-      source: () => obs,
-      period: 1000,
-      mode: AutorefreshMode.CONSTANT
-    });
 
-    expect(received).toBe(null);
+  describe('CONSTANT mode', () => {
+    it('should autorefresh every 1 second', fakeAsync(() => {
+      let received = null;
+      const obs = of('foo')
+        .pipe(
+          tap(it => received = it)
+        );
+      const consumer = autorefresh({
+        source: () => obs,
+        period: 1000,
+        mode: AutorefreshMode.CONSTANT
+      });
 
-    tick(1);
-    expect(received).toBe('foo');
-    received = null;
+      expect(received).toBe(null);
 
-    tick(998);
-    expect(received).toBe(null);
-    tick(1);
-    expect(received).toBe('foo');
-    received = null;
+      tick(1);
+      expect(received).toBe('foo');
+      received = null;
 
-    tick(999);
-    expect(received).toBe(null);
-    tick(1);
-    expect(received).toBe('foo');
+      tick(998);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+      received = null;
 
-    consumer.stop();
-  }));
+      tick(999);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
 
-  it('should reset autorefreseh timer when manual fetch occurs', fakeAsync(() => {
-    let received = null;
-    const obs = of('foo')
-      .pipe(
-        tap(it => received = it)
-      );
-    const consumer = autorefresh({
-      source: () => obs,
-      period: 1000,
-      mode: AutorefreshMode.CONSTANT
-    });
+      consumer.stop();
+    }));
 
-    tick(1500);
-    expect(received).toBe('foo');
-    received = null;
+    it('should autorefresh every 1 second with async request', fakeAsync(() => {
+      let received = null;
+      const obs = of('foo')
+        .pipe(
+          delay(400),
+          tap(it => received = it)
+        );
+      const consumer = autorefresh({
+        source: () => obs,
+        period: 1000,
+        mode: AutorefreshMode.CONSTANT
+      });
 
-    consumer.fetch();
-    tick(1);
-    expect(received).toBe('foo');
-    received = null;
+      expect(received).toBe(null);
 
-    tick(998);
-    expect(received).toBe(null);
-    tick(1);
-    expect(received).toBe('foo');
+      tick(399);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+      received = null;
 
-    consumer.stop();
-  }));
+      tick(999);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+      received = null;
+
+      tick(999);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+
+      consumer.stop();
+    }));
+
+    it('should reset autorefreseh timer when manual fetch occurs', fakeAsync(() => {
+      let received = null;
+      const obs = of('foo')
+        .pipe(
+          tap(it => received = it)
+        );
+      const consumer = autorefresh({
+        source: () => obs,
+        period: 1000,
+        mode: AutorefreshMode.CONSTANT
+      });
+
+      tick(1500);
+      expect(received).toBe('foo');
+      received = null;
+
+      consumer.fetch();
+      tick(1);
+      expect(received).toBe('foo');
+      received = null;
+
+      tick(998);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+
+      consumer.stop();
+    }));
+  });
+
+  describe('SOURCE_DEPENDANT mode', () => {
+    it('should autorefresh every 1 second', fakeAsync(() => {
+      let received = null;
+      const obs = of('foo')
+        .pipe(
+          tap(it => received = it)
+        );
+      const consumer = autorefresh({
+        source: () => obs,
+        period: 1000,
+        mode: AutorefreshMode.SOURCE_DEPENDANT
+      });
+
+      expect(received).toBe('foo');
+      received = null;
+
+      tick(999);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+      received = null;
+
+      tick(999);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+
+      consumer.stop();
+    }));
+
+    it('should autorefresh every 1 second after callback completes', fakeAsync(() => {
+      let received = null;
+      const obs = of('foo')
+        .pipe(
+          delay(500),
+          tap(it => received = it)
+        );
+      const consumer = autorefresh({
+        source: () => obs,
+        period: 1000,
+        mode: AutorefreshMode.SOURCE_DEPENDANT
+      });
+
+      tick(1);
+      expect(received).toBe(null);
+
+      tick(499);
+      expect(received).toBe('foo');
+      received = null;
+
+      tick(1499);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+      received = null;
+
+      tick(1499);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+
+      consumer.stop();
+    }));
+
+    it('should reset autorefreseh timer when manual fetch occurs', fakeAsync(() => {
+      let received = null;
+      const obs = of('foo')
+        .pipe(
+          tap(it => received = it)
+        );
+      const consumer = autorefresh({
+        source: () => obs,
+        period: 1000,
+        mode: AutorefreshMode.SOURCE_DEPENDANT
+      });
+
+      tick(1500);
+      expect(received).toBe('foo');
+      received = null;
+
+      consumer.fetch();
+      expect(received).toBe('foo');
+      received = null;
+
+      tick(999);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+
+      consumer.stop();
+    }));
+
+    it('should reset autorefreseh timer when manual fetch occurs after callback completes', fakeAsync(() => {
+      let received = null;
+      const obs = of('foo')
+        .pipe(
+          delay(500),
+          tap(it => received = it)
+        );
+      const consumer = autorefresh({
+        source: () => obs,
+        period: 1000,
+        mode: AutorefreshMode.SOURCE_DEPENDANT
+      });
+
+      tick(1500);
+      expect(received).toBe('foo');
+      received = null;
+
+      consumer.fetch();
+      tick(499);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+      received = null;
+
+      tick(1499);
+      expect(received).toBe(null);
+      tick(1);
+      expect(received).toBe('foo');
+
+      consumer.stop();
+    }));
+  });
+
 });
