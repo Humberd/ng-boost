0a0389923b731f9caca025fe31c80b623e0b745e
Humberd <humberd2@gmail.com>
2019-06-16 19:03:28 +0200

    Fixed elasticsearch data refresher

--- projects/ng-boost/src/lib/refresher/impl/elasticsearch-pageable-data-refresher.ts
+++ projects/ng-boost/src/lib/refresher/impl/elasticsearch-pageable-data-refresher.ts
@@ -1,16 +1,11 @@
 import { ElasticsearchPage } from '../_models/elasticsearch-page';
-import { PageableDataRefresher } from './pageable-data-refresher';
-import { RefresherConfig } from '../refresher';
+import { PageableDataRefresher, PageOptions } from './pageable-data-refresher';
+import { Refresher, RefresherConfig } from '../refresher';
 import { AutorefreshMode } from '../autorefresh';
+import { Observable } from 'rxjs';
 
+// tslint:disable-next-line:max-line-length
 export abstract class ElasticsearchPageableDataRefresher<SourceData, ParsedData = SourceData> extends PageableDataRefresher<ElasticsearchPage<SourceData>, ParsedData> {
-  private pageInfo: ElasticsearchPage<SourceData>;
-  private _pageNumber = 0;
-  private _pageSize = 10;
-  private _sort = '';
-  private _totalItemsCount = 0;
-  private _searchQuery = '';
-
 
   constructor(config: Partial<RefresherConfig> = {}) {
     super({
@@ -20,76 +15,21 @@ export abstract class ElasticsearchPageableDataRefresher<SourceData, ParsedData 
     });
   }
 
-  get totalItemsCount(): number {
-    return this._totalItemsCount;
+  protected parseSourceData(sourceData: ElasticsearchPage<SourceData>): ParsedData[] {
+    this.totalItemsCount = sourceData.totalElements;
+    return sourceData.content as any as ParsedData[];
   }
 
-  get itemsCount(): number {
-    return this.data.length;
+}
+
+interface Organization {
+  id: number;
+  name: string;
+}
+
+class OrganizationsRefresher extends ElasticsearchPageableDataRefresher<Organization> {
+  protected getPageableDataSource(pageOptions: PageOptions): Observable<ElasticsearchPage<Organization>> | Refresher<any, ElasticsearchPage<Organization>> {
+    return undefined;
   }
 
-  get pageNumber(): number {
-    return this._pageNumber;
-  }
-
-  get pageSize(): number {
-    return this._pageSize;
-  }
-
-  get sort(): string {
-    return this._sort;
-  }
-
-  get searchQuery(): string {
-    return this._searchQuery;
-  }
-
-  nextPage(): void {
-    this._pageNumber++;
-    this.refresh();
-  }
-
-  previousPage(): void {
-    this._pageNumber--;
-    this.refresh();
-  }
-
-  page(pageNumber: number, pageSize: number, sort?: string): void {
-    this._pageNumber = pageNumber;
-    this._pageSize = pageSize;
-    this._sort = sort || '';
-    console.log({pageNumber, pageSize, sort});
-    this.refresh();
-  }
-
-  search(searchQuery: string) {
-    this._searchQuery = searchQuery || '';
-    console.log({searchQuery});
-    this.refresh();
-  }
-
-  /**
-   * Override this function if you want to search by custom field
-   */
-  searchQueryFn(item: ParsedData, searchQuery: string): boolean {
-    return true;
-  }
-
-  firstPage(): void {
-    this._pageNumber = 0;
-    this.refresh();
-  }
-
-
-  lastPage(): void {
-    this._pageNumber = this.pageInfo.totalPages - 1;
-    this.refresh();
-  }
-
-
-// protected abstract loadItems(pageNumber: number, pageSize: number, sort?: string): Observable<ElasticsearchPage<Data>>;
-  //
-  // protected getDataSource(): Observable<ElasticsearchPage<Data>> {
-  //   return this.loadItems(this.pageNumber, this.pageSize, this.sort);
-  // }
 }
