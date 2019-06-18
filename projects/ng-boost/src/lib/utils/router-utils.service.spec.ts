1d91dbd7b0066c5debab9330c087493eb0a3b63f
Humberd <humberd2@gmail.com>
2019-06-18 22:28:47 +0200

    Tests refactor

--- projects/ng-boost/src/lib/utils/router-utils.service.spec.ts
+++ projects/ng-boost/src/lib/utils/router-utils.service.spec.ts
@@ -1,12 +1,30 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { RouterUtilsService } from './router-utils.service';
 import { NoopComponent, NoopModule } from './noop.module';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Router } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { NgZone } from '@angular/core';
 
 describe('RouterUtilsService', () => {
+
+  function initiateModule(routes: Routes) {
+    TestBed.configureTestingModule({
+      providers: [
+        RouterUtilsService
+      ],
+      imports: [
+        NoopModule,
+        RouterTestingModule.withRoutes(routes),
+      ],
+    }).compileComponents();
+
+    const router = TestBed.get(Router);
+    const service = TestBed.get(RouterUtilsService);
+    const ngZone = TestBed.get(NgZone);
+
+    return {router, service, ngZone};
+  }
 
   describe('getParam', () => {
     let service: RouterUtilsService;
@@ -14,106 +32,107 @@ describe('RouterUtilsService', () => {
     let ngZone: NgZone;
 
     beforeEach(() => {
-      TestBed.configureTestingModule({
-        providers: [
-          RouterUtilsService
-        ],
-        imports: [
-          NoopModule,
-          RouterTestingModule.withRoutes([
+      const services = initiateModule([
+        {
+          path: 'sites',
+          children: [
             {
-              path: 'sites',
+              path: ':siteId',
+              component: NoopComponent
+            }
+          ]
+        },
+        {
+          path: 'sla',
+          children: [
+            {
+              path: ':slaId',
               children: [
                 {
-                  path: ':siteId',
+                  path: 'details',
                   component: NoopComponent
                 }
               ]
-            },
-            {
-              path: 'sla',
-              children: [
-                {
-                  path: ':slaId',
-                  children: [
-                    {
-                      path: 'details',
-                      component: NoopComponent
-                    }
-                  ]
-                }
-              ]
             }
-          ]),
-        ],
-      }).compileComponents();
+          ]
+        }
+      ]);
 
-      router = TestBed.get(Router);
-      service = TestBed.get(RouterUtilsService);
-      ngZone = TestBed.get(NgZone);
+      router = services.router;
+      service = services.service;
+      ngZone = services.ngZone;
     });
 
-    it('should get desired params', async () => {
-      await router.navigateByUrl('/sla/abc');
+    it('should get desired params', fakeAsync(() => {
+      ngZone.run(() => {
+        router.navigateByUrl('/sla/abc');
+      });
+      tick(0);
       expect(service.getParam('slaId')).toBe('abc');
 
-      await router.navigateByUrl('/sites/xyz');
+      ngZone.run(() => {
+        router.navigateByUrl('/sites/xyz');
+      });
+      tick(0);
       expect(service.getParam('siteId')).toBe('xyz');
 
-      await router.navigateByUrl('/sla/456/details');
+      ngZone.run(() => {
+        router.navigateByUrl('/sla/456/details');
+      });
+      tick(0);
       expect(service.getParam('slaId')).toBe('456');
-    });
+    }));
 
-    it('should return null for not existing parameters', async () => {
-      await router.navigateByUrl('/sla/abc');
+    it('should return null for not existing parameters', fakeAsync(() => {
+      ngZone.run(() => {
+        router.navigateByUrl('/sla/abc');
+      });
+      tick(0);
       expect(service.getParam('notExistingId')).toBe(undefined);
 
-      await router.navigateByUrl('/sites');
+      ngZone.run(() => {
+        router.navigateByUrl('/sites');
+      });
+      tick(0);
       expect(service.getParam('siteId')).toBe(undefined);
-    });
+    }));
   });
 
   describe('getCurrentRoutesChain', () => {
     let service: RouterUtilsService;
     let router: Router;
+    let ngZone: NgZone;
 
     beforeEach(() => {
-      TestBed.configureTestingModule({
-        providers: [
-          RouterUtilsService
-        ],
-        imports: [
-          NoopModule,
-          RouterTestingModule.withRoutes([
+      const services = initiateModule([
+        {
+          path: 'sites',
+          children: [
             {
-              path: 'sites',
+              path: ':siteId',
+              component: NoopComponent
+            }
+          ]
+        },
+        {
+          path: 'sla',
+          children: [
+            {
+              path: ':slaId',
               children: [
                 {
-                  path: ':siteId',
+                  path: 'details',
                   component: NoopComponent
                 }
               ]
-            },
-            {
-              path: 'sla',
-              children: [
-                {
-                  path: ':slaId',
-                  children: [
-                    {
-                      path: 'details',
-                      component: NoopComponent
-                    }
-                  ]
-                }
-              ]
             }
-          ])
-        ]
-      }).compileComponents();
+          ]
+        }
+      ]);
 
-      router = TestBed.get(Router);
-      service = TestBed.get(RouterUtilsService);
+      router = services.router;
+      service = services.service;
+      ngZone = services.ngZone;
     });
 
     it('should get routes chain', async () => {
