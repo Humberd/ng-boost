c571d3c0ecf3682ba7c76d3b016311a90f59bc73
Humberd <humberd2@gmail.com>
2019-06-20 13:57:44 +0200

    Added Data parser controllers

---
+++ projects/ng-boost/src/lib/controllers/data-parser.controllers.ts
@@ -0,0 +1,51 @@
+import { ChangeDetectorRef, Input, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Destroy$ } from '../utils/destroy';
+import { takeUntil } from 'rxjs/operators';
+
+export abstract class InputDataParserController<InputData, OutputData> {
+  outputData: OutputData;
+  private _data: InputData;
+
+  @Input()
+  set data(data: InputData) {
+    this._data = data;
+    this.outputData = this.parseData(data);
+  }
+
+  protected abstract parseData(data: InputData): OutputData;
+
+}
+
+export abstract class StreamDataParserController<InputData, OutputData> implements OnInit {
+  @Destroy$() protected readonly destroy$ = new Subject();
+  outputData: OutputData;
+
+  /**
+   * You HAVE TO provide a {ChangeDetectorRef} when you have set:
+   * `changeDetection: ChangeDetectionStrategy.OnPush`
+   * Otherwise incoming changes might not be reflected in the view
+   *
+   * @param cdrefff we don't want to have a naming collision with a child class {ChangeDetectorRef}
+   */
+  constructor(private cdrefff?: ChangeDetectorRef) {
+
+  }
+
+  ngOnInit(): void {
+    this.data$()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(data => {
+        this.outputData = this.parseData(data);
+        if (this.cdrefff) {
+          this.cdrefff.markForCheck();
+        }
+      });
+  }
+
+  protected abstract data$(): Observable<InputData>;
+
+  protected abstract parseData(data: InputData): OutputData;
+
+
+}
--- projects/ng-boost/src/lib/utils/destroy.ts
+++ projects/ng-boost/src/lib/utils/destroy.ts
@@ -1,5 +1,32 @@
 import { Subject } from 'rxjs';
 
+/**
+ * Destroys decorated {Subject} in {OnDestoy} phase, so that we don't have to clutter the code with boilerplate
+ * For example:
+ *
+ * Without decorator:
+ * ```
+ * @Component({...})
+ * class Component implements OnDestroy {
+ *   private readonly destroy$ = new Subject();
+ *
+ *   ngOnDestroy() {
+ *     this.destroy$.next();
+ *     this.destroy$.complete();
+ *   }
+ * }
+ * ```
+ *
+ * With decorator:
+ * ```
+ * @Component({...})
+ * class Component implements {
+ *   @Destroy$() private readonly destroy$ = new Subject();
+ * }
+ * ```
+ *
+ * In case of existing {ngOnDestroy} method it will add the destroy feature BEFORE invoking of the existing ones.
+ */
 export function Destroy$(): PropertyDecorator {
   return (target: any, propertyKey) => {
     const destroyFn = function() {
