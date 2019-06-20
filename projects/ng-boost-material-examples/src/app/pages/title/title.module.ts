daca8f3fa73c1ddbc76a8978a369a73aa6c68a38
Humberd <humberd2@gmail.com>
2019-06-20 15:59:28 +0200

    Save commit

--- projects/ng-boost-material-examples/src/app/pages/title/title.module.ts
+++ projects/ng-boost-material-examples/src/app/pages/title/title.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { TitleComponent } from './title/title.component';
+
+const routes: Routes = [{
+  path: '',
+  component: TitleComponent
+}];
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild([{
-      path: '',
-      component: TitleComponent
-    }])
+    RouterModule.forChild(routes)
   ],
   declarations: [TitleComponent]
 })
