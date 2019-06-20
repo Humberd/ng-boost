1c9a7b71e9c29a1073cc99226b36c0a770b49f20
Humberd <humberd2@gmail.com>
2019-06-20 10:46:43 +0200

    Added route params

---
+++ projects/ng-boost/src/lib/route-params/route-param.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { EMPTY, Observable, Subject } from 'rxjs';
+import { RouterUtilsService } from '../utils/router-utils.service';
+import { skip, takeUntil } from 'rxjs/operators';
+import { Destroy$ } from '../utils/destroy';
+
+@Injectable()
+export abstract class RouterParam {
+  @Destroy$() protected readonly destroy$ = new Subject();
+
+  protected value: string;
+  protected value$: Observable<string>;
+  protected valueChange$: Observable<string>;
+
+  constructor(protected routerUtils: RouterUtilsService) {
+    this.value$ = (routerUtils.getParam$(this.fieldName()) || EMPTY)
+      .pipe(takeUntil(this.destroy$));
+
+    this.valueChange$ = this.value$.pipe(skip(1));
+
+    this.value$
+      .subscribe(value => this.value = value);
+  }
+
+  /**
+   * fixme change this to abstract field when this project uses TypeScript version with this change
+   * @see https://github.com/Microsoft/TypeScript/issues/26411
+   */
+  protected abstract fieldName(): string;
+
+}
