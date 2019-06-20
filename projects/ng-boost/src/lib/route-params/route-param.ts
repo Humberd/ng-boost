fc06b7e470c06ccfb0b60d0d90131896e90e80e9
Humberd <humberd2@gmail.com>
2019-06-20 13:03:58 +0200

    Removed unneccessary check

--- projects/ng-boost/src/lib/route-params/route-param.ts
+++ projects/ng-boost/src/lib/route-params/route-param.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { EMPTY, Observable, Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { RouterUtilsService } from '../utils/router-utils.service';
 import { skip, takeUntil } from 'rxjs/operators';
 import { Destroy$ } from '../utils/destroy';
@@ -17,7 +17,7 @@ export abstract class RouteParam {
   readonly valueChange$: Observable<string>;
 
   constructor(protected routerUtils: RouterUtilsService) {
-    this.value$ = (routerUtils.getParam$(this.paramName()) || EMPTY)
+    this.value$ = routerUtils.getParam$(this.paramName())
       .pipe(takeUntil(this.destroy$));
 
     this.valueChange$ = this.value$.pipe(skip(1));
