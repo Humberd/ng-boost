a79e9f08450580bd1ebcb88a98940485e0e153c2
Humberd <humberd2@gmail.com>
2019-06-20 15:31:59 +0200

    Save commit

--- projects/ng-boost-material-examples/src/app/app.module.ts
+++ projects/ng-boost-material-examples/src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([])
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
