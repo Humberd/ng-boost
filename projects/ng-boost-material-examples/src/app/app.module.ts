d89320f8b3d96c27b547b6ed75f1a636df2c942a
Humberd <humberd2@gmail.com>
2019-06-20 15:41:20 +0200

    Added navigation

--- projects/ng-boost-material-examples/src/app/app.module.ts
+++ projects/ng-boost-material-examples/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([]),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   bootstrap: [AppComponent]
 })
