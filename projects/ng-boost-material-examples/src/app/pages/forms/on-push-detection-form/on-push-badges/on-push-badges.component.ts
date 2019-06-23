611eedc882db88a06fca45a2e78e421382fb7a24
Humberd <humberd2@gmail.com>
2019-06-23 21:33:07 +0200

    Removed unneccessary code

--- projects/ng-boost-material-examples/src/app/pages/forms/on-push-detection-form/on-push-badges/on-push-badges.component.ts
+++ projects/ng-boost-material-examples/src/app/pages/forms/on-push-detection-form/on-push-badges/on-push-badges.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnChanges, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormController, FormControllerConfig } from 'ng-boost';
 import { FormControl, Validators } from '@angular/forms';
 
@@ -16,14 +16,10 @@ export interface OnPushBadgesFormValues {
   styleUrls: ['./on-push-badges.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OnPushBadgesComponent extends FormController<OnPushBadgesFormValues> implements OnChanges {
+export class OnPushBadgesComponent extends FormController<OnPushBadgesFormValues> {
   getFormDefinition(): FormControllerConfig<OnPushBadgesFormValues> {
     return {
       badges: new FormControl('', Validators.required)
     };
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes);
   }
 }
