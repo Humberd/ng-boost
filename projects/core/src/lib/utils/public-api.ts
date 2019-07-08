0cceb491a9d3272b32b7730a2faba03b58d25b15
Humberd <humberd2@gmail.com>
2019-07-08 10:09:56 +0200

    Made public utils directory

---
+++ projects/core/src/lib/utils/public-api.ts
@@ -0,0 +1,6 @@
+export * from './destroy';
+export * from './is';
+export * from './noop.module';
+export * from './router-utils.service';
+export * from './rxjs';
+export * from './safe-resolve';
--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -6,6 +6,5 @@ export * from './lib/controllers/public-api';
 export * from './lib/title/public-api';
 export * from './lib/view-switcher/public-api';
 export * from './lib/global-tokens/public-api';
-
-export * from './lib/utils/destroy';
+export * from './lib/utils/public-api';
 
