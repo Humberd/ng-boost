e5ba4e9b6995069cfa9a9c0d76f917052d147ed1
Humberd <humberd2@gmail.com>
2019-07-08 10:03:18 +0200

    Fixed global tokens api export

---
+++ projects/core/src/lib/global-tokens/public-api.ts
@@ -0,0 +1,9 @@
+export { ENVIRONMENT_TOKEN } from './tokens/environment';
+export { LOCAL_STORAGE_TOKEN } from './tokens/local-storage';
+export { LOCATION_TOKEN } from './tokens/location';
+export { SESSION_STORAGE_TOKEN } from './tokens/session-storage';
+
+export * from './tokens/local-storage.mock';
+export * from './tokens/session-storage.mock';
+
+export * from './boost-global-tokens.module';
--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -5,15 +5,7 @@
 export * from './lib/controllers/public-api';
 export * from './lib/title/public-api';
 export * from './lib/view-switcher/public-api';
-
-export * from './lib/global-tokens/boost-global-tokens.module';
-export { ENVIRONMENT_TOKEN } from './lib/global-tokens/tokens/environment';
-export { LOCAL_STORAGE_TOKEN } from './lib/global-tokens/tokens/local-storage';
-export * from './lib/global-tokens/tokens/local-storage.mock';
-export { LOCATION_TOKEN } from './lib/global-tokens/tokens/location';
-export { SESSION_STORAGE_TOKEN } from './lib/global-tokens/tokens/session-storage';
-export * from './lib/global-tokens/tokens/session-storage.mock';
-
+export * from './lib/global-tokens/public-api';
 
 export * from './lib/utils/destroy';
 
