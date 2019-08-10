9b1709bc15d6cce1a9e29c3d8cab49e7ad5ca000
Humberd <humberd2@gmail.com>
2019-08-10 12:25:59 +0200

    Added comment

--- projects/core/src/lib/utils/rxjs.ts
+++ projects/core/src/lib/utils/rxjs.ts
@@ -16,6 +16,10 @@ export function wrapIntoObservable<T>(value: T | Promise<T> | Observable<T>): Ob
 
 /**
  * Reemits stream's last item when {notifier} emits a value.
+ *
+ * When passing a stream from BehaviorSubject make sure to skip the first param:
+ *
+ * `reemitWhen(behaviourSubject.pipe(skip(1)))`
  * @param notifier triggers data to be reemitted.
  */
 export function reemitWhen<T>(notifier: Observable<any>): OperatorFunction<T, T> {
