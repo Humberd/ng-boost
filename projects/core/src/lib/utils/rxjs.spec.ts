a9772ade4dc3546e782130b3b5acc02eb386c464
Humberd <humberd2@gmail.com>
2019-08-10 12:24:41 +0200

    Wrote more tests to rxjs repeatWhen pipe

--- projects/core/src/lib/utils/rxjs.spec.ts
+++ projects/core/src/lib/utils/rxjs.spec.ts
@@ -1,5 +1,6 @@
 import { reemitWhen, wrapIntoObservable } from './rxjs';
-import { of, Subject } from 'rxjs';
+import { BehaviorSubject, of, Subject } from 'rxjs';
+import { skip } from 'rxjs/operators';
 
 describe('rxjs', () => {
 
@@ -37,7 +38,6 @@ describe('rxjs', () => {
   describe('reemitWhen', () => {
 
     it('should reemit data', () => {
-
       const eventEmitter = new Subject();
 
       const dataStream = new Subject();
@@ -60,6 +60,30 @@ describe('rxjs', () => {
       dataStream.next('ghi');
       expect(emittedData).toBe('ghi');
 
+    });
+
+    it('should not throw any errors when emitter is a BehaviorSubject', () => {
+      const eventEmitter = new BehaviorSubject('foobar');
+
+      const dataStream = new Subject();
+
+      let emittedData: any;
+      dataStream
+        .pipe(reemitWhen(eventEmitter.pipe(skip(1))))
+        .subscribe(value => emittedData = value);
+
+      dataStream.next('abc');
+      expect(emittedData).toBe('abc');
+
+      dataStream.next('def');
+      expect(emittedData).toBe('def');
+      emittedData = null;
+
+      eventEmitter.next('event-data'); // <--- triggerint reemit
+      expect(emittedData).toBe('def');
+
+      dataStream.next('ghi');
+      expect(emittedData).toBe('ghi');
     });
 
   });
