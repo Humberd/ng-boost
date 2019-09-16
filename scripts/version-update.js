ac4871128eec6f5fd11ff93609ac334af7406929
Humberd <humberd2@gmail.com>
2019-09-16 21:07:38 +0200

    Save commit

--- scripts/version-update.js
+++ scripts/version-update.js
@@ -5,7 +5,20 @@ const PROJECTS = [
   'material',
 ];
 
-console.log(hasUncommitedFiles());
+if (hasUncommitedFiles()) {
+  throw Error('You have some uncommited files');
+}
+shell.cd('projects');
+
+for (const project of PROJECTS) {
+  shell.cd(`projects/${project}`);
+
+  shell.exec('npm version prerelease --preid=beta');
+
+  shell.cd('../../');
+
+  shell.exec(`ng build ${project}`);
+}
 
 function hasUncommitedFiles() {
   return !!shell.exec('git status --porcelain', {silent: true}).stdout;
