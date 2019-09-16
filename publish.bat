ec6284cc4244fc180ee1ab558f4c2bb180a36a17
Humberd <humberd2@gmail.com>
2019-09-16 12:34:47 +0200

    Save commit

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
-npm version prerelease --preid=beta -m "Version: %s"
-npm run test-core:ci
-npm run build-core
-git push --follow-tags
+call npm run version:beta-core
+call npm run test-core:ci
+call npm run build-core
+call git push --follow-tags
 
 
