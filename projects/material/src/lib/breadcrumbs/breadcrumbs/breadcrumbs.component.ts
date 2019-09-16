f486042e531e549479ca435f86fd163628ff525e
Humberd <humberd2@gmail.com>
2019-09-16 21:40:38 +0200

    Breadcrumbs now have no encapsulation

--- projects/material/src/lib/breadcrumbs/breadcrumbs/breadcrumbs.component.ts
+++ projects/material/src/lib/breadcrumbs/breadcrumbs/breadcrumbs.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
 import { BoostBreadcrumbsService, Breadcrumb } from '@ng-boost/core';
 
 @Component({
   selector: 'boost-mat-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.scss'],
+  encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BreadcrumbsComponent {
