2cd46f1ac37e1a8e29af7f08c0167d3dcb8eb03c
Humberd <humberd2@gmail.com>
2019-09-16 21:39:50 +0200

    Breadcrumbs can now have breadcrumbs as an input

--- projects/material/src/lib/breadcrumbs/breadcrumbs/breadcrumbs.component.ts
+++ projects/material/src/lib/breadcrumbs/breadcrumbs/breadcrumbs.component.ts
@@ -1,13 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { BoostBreadcrumbsService } from '@ng-boost/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { BoostBreadcrumbsService, Breadcrumb } from '@ng-boost/core';
 
 @Component({
   selector: 'boost-mat-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BreadcrumbsComponent {
+  @Input() breadcrumbs: Breadcrumb[];
 
   constructor(public breadcrumbsService: BoostBreadcrumbsService) {
   }
