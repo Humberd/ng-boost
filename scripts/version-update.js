55dd86f3dc349657fa8bc29350e49c25498530ba
Humberd <humberd2@gmail.com>
2019-09-16 21:19:24 +0200

    Version: v0.0.1-beta.11

--- scripts/version-update.js
+++ scripts/version-update.js
@@ -11,15 +11,21 @@ if (!DEBUG_MODE && hasUncommitedFiles()) {
   throw Error('You have some uncommited files');
 }
 
+let currentVersion;
+
 for (const project of PROJECTS) {
   shell.cd(`./projects/${project}`);
 
-  shell.exec('npm version prerelease --preid=beta -m "Version: %s"');
+  currentVersion = shell.exec('npm version prerelease --preid=beta').stdout;
 
   shell.cd('../../');
 
   shell.exec(`ng build ${project}`);
 }
+
+shell.exec('git add -A');
+
+shell.exec(`git commit -m "Version: ${currentVersion}"`);
 
 function hasUncommitedFiles() {
   return !!shell.exec('git status --porcelain', {silent: true}).stdout;
