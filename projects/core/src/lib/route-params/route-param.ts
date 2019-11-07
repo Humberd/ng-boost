99aea437958a9054b379025708ae9783b77dafef
Maciej Sawicki <humberd2@gmail.com>
2019-11-07 08:54:20 +0100

    Update route-param.ts

--- projects/core/src/lib/route-params/route-param.ts
+++ projects/core/src/lib/route-params/route-param.ts
@@ -4,6 +4,30 @@ import { RouterUtilsService } from '../utils/router-utils.service';
 import { map, skip, takeUntil } from 'rxjs/operators';
 import { Destroy$ } from '../utils/destroy';
 
+/**
+ * Allows you to make the Router param statically typed across many components.
+ * Normally you get params by dynamic names like this:
+ *
+ * constructor(private router: Router) {
+ *    const userId = router.snapshot.paramMap.get('userId');
+ * }
+ * However, you need to use this pattern in every component that has userId access
+ *
+ * This class allows to do stuff like this:
+ * @Injectable()
+ * export class UserIdParam extends RouteParam {
+ *   paramName() {
+ *      return 'userId';
+ *   }
+ * }
+ *
+ * And then in the component:
+ * constructor(private userIdParam: UserIdParam) {
+ *   const userId = userIdParam.value;
+ * }
+ *
+ * This allows to make the param statically typed across many components
+ */
 @Injectable()
 export abstract class RouteParam<T = string> {
   @Destroy$() protected readonly destroy$ = new Subject();
